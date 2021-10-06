392382b397e82e1f27ec7971dbdab7d3ad091007
daniel <tredeneo@hotmail.com>
2021-10-06 12:57:52 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # porcentage_language_dir
+
+program for calculate porcentage of a programming language in a directory
+
+inspirate by [gitgub linguist](https://github.com/github/linguist/) but work in non-git directories
+
+the list of languages is a CONST in the code(so edit it for add what you need)
+
+not have dependecies for print porcentages
+
+if a args -p or --plot will save a png pie chart using ```matplotlib```
---
+++ porcentagem_linguagens.py
@@ -0,0 +1,100 @@
+import os
+import argparse
+
+
+LANGUAGES = {
+    "rs": "rust",
+    "java": "java",
+    "c": "c",
+    "h": "c",
+    "cpp": "c++",
+    "hpp": "c++",
+    "py": "python",
+    "dart": "dart",
+    "html": "html",
+    "makefile": "makefile",
+}
+
+
+def save_img(porcents):
+    import matplotlib.pyplot as plt
+
+    plt.pie(list(porcents.values()), labels=porcents.keys())
+    plt.savefig("porcentagem_linguagens.png", transparent=True)
+
+
+def print_languages(porcents: dict):
+    porcents = dict(sorted(porcents.items(), key=lambda item: item[1], reverse=True))
+    lenght = 0
+    for i in porcents:
+        if len(i) > lenght:
+            lenght = len(i)
+
+    for i in porcents:
+        print(f"{i.ljust(lenght,'_')}:{porcents[i]}")
+
+
+def get_files_size() -> (dict, int):
+    dir = os.path.dirname(__file__)
+    types = {}
+    total_size = 0
+    for dirs, _, filenames in os.walk(dir):
+        for actual_file in filenames:
+            file = os.path.join(dirs, actual_file)
+            if actual_file == os.path.basename(__file__):
+                continue
+            file_type = actual_file.split(".")[-1].lower()
+            if file_type not in (LANGUAGES.keys()):
+                continue
+            tmp_size = os.path.getsize(file)
+            total_size += tmp_size
+            if file_type not in types:
+                types[file_type] = tmp_size
+            else:
+                types[file_type] += tmp_size
+    return types, total_size
+
+
+def calc_porcent(freq: dict, total: int) -> dict:
+    porcentagem = {}
+    for i in freq:
+        porcentagem[i] = 100 * int([freq[i]][0]) / total
+    return porcentagem
+
+
+def merge_name_extension(porcent: dict):
+    porcent_merged = {}
+    for i in porcent:
+        if LANGUAGES[i] not in porcent_merged:
+            porcent_merged[LANGUAGES[i]] = porcent[i]
+        else:
+            porcent_merged[LANGUAGES[i]] += porcent[i]
+    for i in porcent_merged:
+        porcent_merged[i] = round(porcent_merged[i], 2)
+
+    return porcent_merged
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Process porcentage of languages in the directory, include subdirecties."
+    )
+    parser.add_argument(
+        "-p", "--plot", help="save a img with matplotlib", action="store_true"
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    freq, total = get_files_size()
+    porcent = calc_porcent(freq, total)
+
+    list = merge_name_extension(porcent)
+    print_languages(list)
+    if args.plot:
+        save_img(list)
+
+
+if __name__ == "__main__":
+    main()
